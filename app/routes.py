cc341361919ea560f38cf117b99e618321279973
dessHub <deskip95@gmail.com>
2017-01-24 14:21:16 +0300

    configure mongodb database

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,14 @@
 from flask import render_template, redirect, url_for
 from app import app
-#from models.forms import UserForm
-#from collection.documents import Book
+from forms import StoreForm
+from document import Store
 
+@app.route('/')                   #Route to return landing page(index page)
+def index(page=1):
+    title = u'Stores list'
+    stores = Store.query.paginate(page=page, per_page=5)
 
-@app.route('/')   #Route to return landing page(index page)
-def index():
-    return render_template('index.html')
+    return render_template('index.html', stores=stores, title=title)
 
 @app.route('/login')
 def login():
@@ -16,10 +18,18 @@ def login():
 def signup():
     return render_template('sign_up.html')
 
-@app.route('/create')
-def create():
-    return render_template('user.html')
+@app.route('/store_form')
+def store_form():
+    return render_template('store_form.html', form=form)
 
 @app.route('/store')
 def store():
     return render_template('store.html')
+
+@app.route('/api/new', methods=['GET', 'POST'])
+def new_store():
+    form = StoreForm()
+    if form.validate_on_submit():
+        form.save()
+        return redirect(url_for('new_store'))
+    return render_template('store_form.html', form=form)
